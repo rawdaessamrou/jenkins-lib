f37f068c5b265d5ff2f4923227d0ec8d96f13589
Rawda Essam <159459253+rawdaessamrou@users.noreply.github.com>
2026-04-30 16:30:36 +0300

    Refactor buildImage function for specific directory

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,4 +1,5 @@
-def call(String imageName) {
-    echo "Building Docker image: ${imageName}"
-    sh "docker build -t ${imageName} ."
+def call() {
+    dir('CI-CD/Lab 23 - CICD Pipeline Implementation with Jenkins Agents and Shared Libraries/Jenkins_App') {
+        sh "docker build -t my-registry/jenkins-app:${env.BUILD_ID} ."
+    }
 }
