8c66a0e749d629792360aaf2343b8b767213ed75
Rawda Essam <159459253+rawdaessamrou@users.noreply.github.com>
2026-04-30 16:39:46 +0300

    Update scanImage function to use Docker for scanning

--- vars/scanImage.groovy
+++ vars/scanImage.groovy
@@ -1,4 +1,4 @@
 def call(String imageName) {
-    echo "--- Scanning Image for Vulnerabilities ---"
-    sh "trivy image ${imageName} || echo 'Scan finished with issues'"
+    echo "--- Scanning Image: ${imageName} ---"
+    sh "docker run --rm -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy:latest image ${imageName}"
 }
