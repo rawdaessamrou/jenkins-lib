96697d26fbea03a258b59093abdfe3410e3ddec8
Rawda Essam <159459253+rawdaessamrou@users.noreply.github.com>
2026-04-30 16:34:25 +0300

    Modify buildImage function to accept imageName parameter

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,5 +1,7 @@
-def call() {
+def call(String imageName) { // Added String imageName here
+    echo "--- Building Docker Image: ${imageName} ---"
+    
     dir('CI-CD/Lab 23 - CICD Pipeline Implementation with Jenkins Agents and Shared Libraries/Jenkins_App') {
-        sh "docker build -t my-registry/jenkins-app:${env.BUILD_ID} ."
+        sh "docker build -t ${imageName} ."
     }
 }
