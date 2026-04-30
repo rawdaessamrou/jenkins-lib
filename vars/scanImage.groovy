93f35f8ae46bec84c1a70eaee824d2fe181cdd66
Rawda Essam <159459253+rawdaessamrou@users.noreply.github.com>
2026-04-30 16:48:21 +0300

    Update scanImage function to use trivy command

--- vars/scanImage.groovy
+++ vars/scanImage.groovy
@@ -1,4 +1,5 @@
 def call(String imageName) {
     echo "--- Scanning Image: ${imageName} ---"
-    sh "docker run --rm -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy:latest image ${imageName}"
+    sh "trivy image --debug --download-java-db-only" 
+    sh "trivy image ${imageName}"
 }
