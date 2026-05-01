fc900098da48197e6ef52c636c81040b88a3128c
Rawda Essam <159459253+rawdaessamrou@users.noreply.github.com>
2026-05-02 01:24:28 +0300

    Enhance deployment script with search for deployment.yaml
    Added echo and find commands to locate deployment.yaml.

--- vars/deployOnK8s.groovy
+++ vars/deployOnK8s.groovy
@@ -1,8 +1,11 @@
 def call() {
     withCredentials([file(credentialsId: 'kubeconfig', variable: 'KUBECONFIG_PATH')]) {
         sh '''
+            echo "Searching for deployment.yaml in: $WORKSPACE"
+            find "$WORKSPACE" -name "deployment.yaml"
+            
             export KUBECONFIG="$KUBECONFIG_PATH"
-            kubectl apply -f "${WORKSPACE}/deployment.yaml"
+            kubectl apply -f "$WORKSPACE/deployment.yaml"
         '''
-    } 
-} 
+    }
+}
