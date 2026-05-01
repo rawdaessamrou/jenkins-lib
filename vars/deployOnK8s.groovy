9383fa051a3ba4dc28bb70ba14cc7b6817085b0f
Rawda Essam <159459253+rawdaessamrou@users.noreply.github.com>
2026-05-02 00:19:51 +0300

    Refactor Kubernetes deployment script

--- vars/deployOnK8s.groovy
+++ vars/deployOnK8s.groovy
@@ -1,6 +1,6 @@
-def call(String kubeConfigCredId) {
-    echo "--- Deploying to Kubernetes ---"
-    withCredentials([file(credentialsId: kubeConfigCredId, variable: 'KUBECONFIG')]) {
-        sh "kubectl --kubeconfig=${KUBECONFIG} apply -f k8s/"
-    }
+withCredentials([file(credentialsId: 'kubeconfig', variable: 'KUBECONFIG')]) {
+    sh '''
+        export KUBECONFIG=$KUBECONFIG
+        /usr/bin/kubectl apply -f k8s/
+    '''
 }
