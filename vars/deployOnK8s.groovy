003506066efc30645052d5f5404facb1f4658fcf
Rawda Essam <159459253+rawdaessamrou@users.noreply.github.com>
2026-05-02 01:28:52 +0300

    Modify deployment script to find deployment.yaml
    Updated deployment script to search for deployment.yaml in a specific path and handle errors if not found.

--- vars/deployOnK8s.groovy
+++ vars/deployOnK8s.groovy
@@ -1,11 +1,16 @@
 def call() {
     withCredentials([file(credentialsId: 'kubeconfig', variable: 'KUBECONFIG_PATH')]) {
         sh '''
-            echo "Searching for deployment.yaml in: $WORKSPACE"
-            find "$WORKSPACE" -name "deployment.yaml"
-            
             export KUBECONFIG="$KUBECONFIG_PATH"
-            kubectl apply -f "$WORKSPACE/deployment.yaml"
+            DEPLOY_PATH=$(find . -path "*Lab 23*/Jenkins_App/deployment.yaml" | head -n 1)
+
+            if [ -z "$DEPLOY_PATH" ]; then
+                echo "ERROR: deployment.yaml not found for Lab 23"
+                exit 1
+            fi
+
+            echo "Found deployment file at: $DEPLOY_PATH"
+            kubectl apply -f "$DEPLOY_PATH"
         '''
     }
 }
